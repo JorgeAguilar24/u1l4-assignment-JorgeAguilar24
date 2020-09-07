5a3d35c8757195314fcc6d76a55c436cef83b985
JorgeAguilar24 <70876154+JorgeAguilar24@users.noreply.github.com>
2020-09-06 23:44:35 -0700

    Update AnalogClock.java
    Completed All Problems

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,12 +7,19 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
-
+        int hourHand = scanner.nextInt();
+        int hours;
+        int degreesPast;
+        int minutesPast;
+        int minuteHandDegrees;
         /*
          *  your code goes here
          */
-
+            hours = hourHand / 30;
+            degreesPast = hours * 30;
+            minutesPast = hourHand - degreesPast;
+            minuteHandDegrees = (30 - minutesPast) * 6;
+            System.out.print(minuteHandDegrees);
         // closing the scanner object
         scanner.close();
     }
