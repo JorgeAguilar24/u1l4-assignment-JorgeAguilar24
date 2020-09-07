48cf4d6f84a980b874d1b8aa5783fd97c682ef51
JorgeAguilar24 <70876154+JorgeAguilar24@users.noreply.github.com>
2020-09-06 22:50:01 -0700

    Update Timestamps.java
    Completed Timestamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -13,12 +13,26 @@ class Timestamps {
         int hours2 = scanner.nextInt();
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
+        
 
 
         /*
          *  your code goes here
          */
+        int deltaH;
+        int deltaM;
+        int deltaS;
+        int secsDeltaH;
+        int secsDeltaM;
+        int totalSecs; 
 
+        deltaH = hours2 - hours1;
+        deltaM = minutes2 - minutes1;
+        deltaS = seconds2 - seconds1;
+        secsDeltaH = deltaH * 3600;
+        secsDeltaM = deltaM * 60;
+        totalSecs = secsDeltaH + secsDeltaM + deltaS;
+        System.out.print(totalSecs);
 
         // closing the scanner object
         scanner.close();
