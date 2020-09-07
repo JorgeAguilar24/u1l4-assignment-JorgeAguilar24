d345dfe7a73cf38fe3ce4862a408cd15a2a646bb
JorgeAguilar24 <70876154+JorgeAguilar24@users.noreply.github.com>
2020-09-06 23:06:55 -0700

    Update StudentDesks.java
    Completed StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -7,12 +7,18 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        int students1 = scanner.nextInt();
+        int students2 = scanner.nextInt();
+        int students3 = scanner.nextInt();
 
         /*
          *  your code goes here
          */
-
+          int totalStu;
+          int totalDesks;
+          totalStu = students1 + students2 + students3;
+          totalDesks = ((totalStu / 2) + (totalStu % 2));
+          System.out.print(totalDesks);
 
         // closing the scanner object
         scanner.close();
